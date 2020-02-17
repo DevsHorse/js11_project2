c345d2b9763013bddbad1debb00bbbb10abcb9ce
Vladislav Holovko <lobsterponi@gmail.com>
2020-02-17 22:21:51 +0200

    v1

--- js/main.js
+++ js/main.js
@@ -8,30 +8,34 @@ window.addEventListener('DOMContentLoaded', function() {
             timeSeconds = document.querySelector('#timer-seconds');
 
         function getTimeRemaining() {
+        
             let dateStop = new Date(deadline).getTime(),
             dateNow = new Date().getTime(),
-            timeRemaining = (dateStop - dateNow) / 1000;
-            console.log(dateNow)
-            if (timeRemaining <= 0) {
+            timeRemaining = (dateStop - dateNow) / 1000,
+            lefthours = 24 + (Math.floor(timeRemaining / 60 / 60) - 24);
+
+            if (lefthours < 0) {
+                deadlineCounter();
                 return;
             }
-
+            
             let seconds = Math.floor(timeRemaining % 60),
             minutes = Math.floor((timeRemaining / 60) % 60),
             hours = Math.floor(timeRemaining / 60 / 60);
-            console.log(24 + (Math.floor(timeRemaining / 60 / 60) - 24))
-                function a(time, ...arr) {
+            
+                function getTime(time, ...arr) {
                     arr = arr.map( (item) => item < 10 ? '0' + item : '' + item);
                     let [seconds, minutes, hours] = [...arr];
                     return { timeRemaining, seconds, minutes, hours };
                 }
 
-            return a(timeRemaining, seconds, minutes, hours);
+            return getTime(timeRemaining, seconds, minutes, hours);
         }   
 
         const updateClock = setInterval(function() {
             let timer = getTimeRemaining();
             if (timer === undefined) {
+                clearInterval(updateClock);
                 return;
             }
             timeHours.textContent = timer.hours;
@@ -44,10 +48,13 @@ window.addEventListener('DOMContentLoaded', function() {
         }, 1000);
     }
 
-    
+    function deadlineCounter() {
+        let date = new Date();
+        let deadline = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate() + 1}`;
+        return deadline; 
+    }
 
-    countTimer('2020-02-18T00:00:00');
-    // setInterval(countTimer, 1000, '01 july 2019');
+    countTimer(deadlineCounter());
 
 
 
